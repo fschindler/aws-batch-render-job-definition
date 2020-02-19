46bd2e0c63f853c46365a24ea406a03dbb5625e9
Felix Schindler <mail@felixschindler.de>
2020-02-19 21:14:23 +0100

    prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,48 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import path from 'path'
+import fs from 'fs'
+import tmp from 'tmp'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // Get inputs
+    const jobDefinitionFile = core.getInput('job-definition', {required: true})
+    const imageURI = core.getInput('image', {required: true})
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // Parse the job definition
+    const jobDefPath = path.isAbsolute(jobDefinitionFile)
+      ? jobDefinitionFile
+      : path.join(process.env.GITHUB_WORKSPACE || '', jobDefinitionFile)
+    if (!fs.existsSync(jobDefPath)) {
+      throw new Error(
+        `Job definition file does not exist: ${jobDefinitionFile}`
+      )
+    }
+    const buffer = fs.readFileSync(jobDefPath)
+    const jobDefContents = JSON.parse(buffer.toString())
 
-    core.setOutput('time', new Date().toTimeString())
+    // Insert the image URI
+    if (
+      typeof jobDefContents.containerProperties !== 'object' ||
+      !jobDefContents.containerProperties.hasOwnProperty('image')
+    ) {
+      throw new Error(
+        'Invalid job definition format: containerProperties section is not present, is not an object or has no property image'
+      )
+    }
+    jobDefContents.containerProperties.image = imageURI
+
+    // Write out a new task definition file
+    const updatedJobDefFile = tmp.fileSync({
+      dir: process.env.RUNNER_TEMP,
+      prefix: 'job-definition-',
+      postfix: '.json',
+      keep: true,
+      discardDescriptor: true
+    })
+    const newJobDefContents = JSON.stringify(jobDefContents, null, 2)
+    fs.writeFileSync(updatedJobDefFile.name, newJobDefContents)
+    core.setOutput('job-definition', updatedJobDefFile.name)
   } catch (error) {
     core.setFailed(error.message)
   }
